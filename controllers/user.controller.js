d57188cd6b3c1d0430810fa9b3cea33e3da4e0f6
abm6 <55770187+abm6@users.noreply.github.com>
2022-07-11 15:15:09 +0530

    handle errors

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,6 +2,7 @@ const sendCount = async (req, res) => {
 	const data = req.body.data;
 	let totalCount = 0;
 
+ 
 	const answer = {
 		isSuccess: true,
 		user_id: 'abhisek_mandal_11081999',
@@ -12,27 +13,35 @@ const sendCount = async (req, res) => {
 		alphabets: [],
 	};
 
-	data.forEach((item) => {
-		for (let i = 0; i < item.length; i++) {
-			console.log(item[i]);
-			if (item[i] >= 'A' && item[i] <= 'Z') {
-				if (answer.alphabets.indexOf(item) === -1) answer.alphabets.push(item);
-				totalCount++;
-			}
-			if (item[i] >= 'a' && item[i] <= 'z') {
-				if (answer.alphabets.indexOf(item) === -1) answer.alphabets.push(item);
-				totalCount++;
-			}
-			if (item[i] >= '0' && item[i] <= '9') {
-				if (answer.numbers.indexOf(item) === -1) answer.numbers.push(item);
-				totalCount++;
-			}
-		}
-	});
 
-	answer.count = totalCount;
+  try {
+    data.forEach((item) => {
+      for (let i = 0; i < item.length; i++) {
+        console.log(item[i]);
+        if (item[i] >= 'A' && item[i] <= 'Z') {
+          if (answer.alphabets.indexOf(item) === -1) answer.alphabets.push(item);
+          totalCount++;
+        }
+        if (item[i] >= 'a' && item[i] <= 'z') {
+          if (answer.alphabets.indexOf(item) === -1) answer.alphabets.push(item);
+          totalCount++;
+        }
+        if (item[i] >= '0' && item[i] <= '9') {
+          if (answer.numbers.indexOf(item) === -1) answer.numbers.push(item);
+          totalCount++;
+        }
+      }
+    });
+  
+    answer.count = totalCount;
+  
+    if (data.length == 0) answer.isSuccess = false;  
+  }
+  catch (err) {
+    res.sendStatus(404);
+  }
 
-	if (data.length == 0) answer.isSuccess = false;
+	
 
 	res.send(answer);
 };
