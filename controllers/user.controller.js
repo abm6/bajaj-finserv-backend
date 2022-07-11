417002eaecffa4451dd821ef28fbd399e8494d56
abm6 <55770187+abm6@users.noreply.github.com>
2022-07-11 14:07:06 +0530

    updated

---
+++ controllers/user.controller.js
@@ -0,0 +1,47 @@
+const sendCount = async (req, res) => {
+	const data = req.body.data;
+	let totalCount = 0;
+
+	const answer = {
+		isSuccess: true,
+		user_id: 'abhisek_mandal_11081999',
+		count: '',
+		email: '1929194@kiit.ac.in',
+		roll_number: '1929194',
+		numbers: [],
+		alphabets: [],
+	};
+
+	data.forEach((item) => {
+		for (let i = 0; i < item.length; i++) {
+			console.log(item[i]);
+			if (item[i] >= 'A' && item[i] <= 'Z') {
+				if (answer.alphabets.indexOf(item) === -1) answer.alphabets.push(item);
+				totalCount++;
+			}
+			if (item[i] >= 'a' && item[i] <= 'z') {
+				if (answer.alphabets.indexOf(item) === -1) answer.alphabets.push(item);
+				totalCount++;
+			}
+			if (item[i] >= '0' && item[i] <= '9') {
+				if (answer.numbers.indexOf(item) === -1) answer.numbers.push(item);
+				totalCount++;
+			}
+		}
+	});
+
+	answer.count = totalCount;
+
+	if (data.length == 0) answer.isSuccess = false;
+
+	res.send(answer);
+};
+
+const deflect = (req, res) => {
+	res.redirect('/challenge');
+};
+
+module.exports = {
+	sendCount,
+	deflect,
+};
